d29304b47ab3ba6cc96605ad51454fc54dee21d8
Guilherme <guiferreiramoraes@outlook.com>
2025-02-16 01:48:19 -0300

    Initial commit

---
+++ private_dot_config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,17 @@
+-- options
+vim.opt.cursorline = false
+vim.opt.number = true
+vim.opt.tabstop = 2
+vim.opt.shiftround = true
+vim.opt.shiftwidth = 2
+vim.opt.ruler = true
+
+--colors
+vim.cmd [[ colorscheme sorbet ]]
+vim.opt.guicursor = 'n-v-c-i:block-Cursor/lCursor'
+
+-- mappings 
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.keymap.set('n', '<Leader>w', ':write<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>h', ':nohlsearch<CR>')
